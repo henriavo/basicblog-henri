ed43ad53fe3b29f9f171e936812ca07259ef06ec
Henri Idrovo <henri.idrovo@gmail.com>
2015-11-26 16:41:46 -0600

    problem set 3. test successful!

--- main.py
+++ main.py
@@ -32,11 +32,42 @@ class NewPostHandler(Handler):
 	def get(self):
 		self.renderIt("newpost.html")
 
+	def post(self):
+		theSubject = self.request.get("subject")
+		thePost = self.request.get("content")
+
+		if (not theSubject.strip() or not thePost.strip()):
+			error = "subject and content, please!"
+			self.renderIt("newpost.html", error=error, subject=theSubject, content=thePost)
+		else:
+			post = BlogEntry(subject=theSubject, blog=thePost)
+
+			post.put()
+			
+			print "THE ID: " + str(post.key().id())
+			theBlogId = str(post.key().id())
+
+			self.redirect("/blog/%s" % theBlogId)
+
+class PostHandler(Handler):
+	def get(self, post_id):
+		p = BlogEntry.get_by_id(long(post_id))
+		if p:
+			posts =[p]
+			self.renderIt("mainpage.html", posts=posts)
+
+
+
 class MainPageHandler(Handler):
 	def get(self):
-		self.renderIt("mainpage.html")
+		posts = db.GqlQuery("SELECT * FROM BlogEntry" 
+							" ORDER BY created DESC")
+		
+		self.renderIt("mainpage.html", posts=posts)
+
 
 app = webapp2.WSGIApplication([
 	('/blog/newpost', NewPostHandler),
-	('/blog', MainPageHandler)
+	('/blog', MainPageHandler),
+	('/blog/([0-9]+)', PostHandler)
 ], debug=True)
